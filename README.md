206c3c90afe56fe599c48297bf98c7a5104769c9
Innei <tukon479@gmail.com>
2022-02-11 16:26:01 +0800

    init;

---
+++ README.md
@@ -0,0 +1,1 @@
+Reproduction: vite-vue-scoped-global
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<style scoped>
+.editor >>> .e {
+  background: #000;
+}
+.editor :deep(.a) {
+  background: #000;
+}
+</style>
+
+<style scoped>
+.editor :global(.d) {
+  background: #000;
+}
+</style>
+
+<style scoped>
+:global(.ee) {
+  background: #000;
+}
+</style>
